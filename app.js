fb2126e9118d5cd901ef3ede0fed902c01ec4ca5
zhounie <iszhounie@aliyun.com>
2020-10-31 00:27:16 +0800

    添加跨域

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa')
 const router = require('./router/index')
 const bodyParser = require('koa-bodyparser')
+const cors = require('koa2-cors')
+
 require('./db/index')
 
 
 const app = new Koa()
+
+app.use(cors())
 
 app.use(bodyParser())
 
