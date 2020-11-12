825b2e1ae5808da5cc2c24b851e66363e6aa1ca2
zn <iszhounie@aliyun.com>
2020-11-12 10:05:54 +0800

    修改数据库

--- db/index.js
+++ db/index.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize')
 
 const config = {
     host: '81.69.28.107',
-    port: '3306',
+    port: '9901',
     dialect: 'mysql'
 }
 
-const sequelize = new Sequelize('blog', 'root', '123456', config)
+const sequelize = new Sequelize('blog', 'root', 'zn19981130', config)
 
 module.exports = sequelize
