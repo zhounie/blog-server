da195a391fb6c1c2643778a3cd4b58e2c97acfe2
zhounie <iszhounie@aliyun.com>
2020-11-15 16:44:17 +0800

    忽略路由

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
 
 
 module.exports = {
-    IGNORE_TOKEN_ROUTES: ['/user/login', '/user/insert'] 
+    IGNORE_TOKEN_ROUTES: [
+        '/user/login',
+        '/user/insert',
+        '/book/getBookList',
+        '/blog/getBlogList',
+    ] 
 }
-    
-
